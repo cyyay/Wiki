fb1a52d929da484f58e9ebba17cfc76a50b67574
Cy <cyyyyxoxo@gmail.com>
2024-08-02 18:54:52 +0530

    added all features

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.page, name="page"),
+    path("encyclopedia/create.html", views.create, name="create"),
+    path("edit/<str:name>", views.edit, name="edit"),
+    path("random/", views.random_page, name='random')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,19 @@
+import random
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 from . import util
+import markdown2
 
+class NewPage(forms.Form):
+    title= forms.CharField(label="Enter the title:", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', 'style': 'width: 50%;'}))
+    content= forms.CharField(label="Enter the content:",  widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 10, 'placeholder': 'Content', 'style': 'width: 50%;'}))
 
-
+entries = util.list_entries()
 
 def index(request):
     q = request.GET.get('q')
-    entries = util.list_entries()
     if q:
         if q.lower() in [s.lower() for s in entries]:
             return redirect('page', name=q)
@@ -31,5 +36,50 @@ def index(request):
 def page(request, name):
     return render(request, "encyclopedia/page.html", {
         "name": name,
-        "body": util.get_entry(name)
+        "body": markdown2.markdown(util.get_entry(name))
     })
+
+def create(request):
+    if request.method == "POST":
+        form=NewPage(request.POST)
+
+        if form.is_valid():
+            t= form.cleaned_data["title"]
+            c= form.cleaned_data["content"]
+
+            if t not in entries:
+                util.save_entry(t, c)
+                return HttpResponseRedirect(reverse('page', args=[t]))
+            else:
+                return render(request, "encyclopedia/create.html", {
+                    "form": form,
+                    "error": True
+                })
+    else:
+        form= NewPage()
+
+
+    return render (request, "encyclopedia/create.html", {
+        "form": form
+    })
+
+def edit(request, name):
+    if request.method == "POST":
+        form= NewPage(request.POST)
+        if form.is_valid():
+            content= form.cleaned_data["content"]
+            util.save_entry(name, content)
+            return HttpResponseRedirect(reverse('page', args=[name]))
+    else:
+        initial_data= {'title': name,   'content': util.get_entry(name)}
+        form= NewPage(initial=initial_data)
+
+    return render(request, "encyclopedia/edit.html", {
+        "form": form,
+        "title": name
+    })
+
+def random_page(request):
+    entriees = util.list_entries()
+    random_entry = random.choice(entriees)
+    return HttpResponseRedirect(reverse('page', args=[random_entry]))
